67f48fb335ef0b922a78ede6a63825be29019fac
gitdevixx <developerixx@gmail.com>
2019-10-29 13:09:56 +0900

    1 file modified

--- src/app/vixx/leo/leo.component.ts
+++ src/app/vixx/leo/leo.component.ts
@@ -1,18 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-leo',
   templateUrl: './leo.component.html',
   styleUrls: [
     './leo.component.css',
-    './external-source/css/bootstrap.css',
-    './external-source/css/stylish-portfolio.css',
-    './external-source/css/simple-line-icons.css'
+    './external-source/bootstrap/css/bootstrap.css',
+    './external-source/simple-line/css/stylish-portfolio.css',
+    './external-source/simple-line/css/simple-line-icons.css'
   ]
 })
 export class LeoComponent implements OnInit {
+  vixxForm;
+  langDatas = {pageName: 'VIXX'};
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private translate: TranslateService
+  ) { 
+    this.vixxForm = this.formBuilder.group({
+      id: '',
+      text: ''
+    });
+
+    // this.pageName = {testTitle: 'VIXX'};
+  }
+
+  resetForm() {
+    this.vixxForm.reset();
+  }
+
+  onSubmit(formDatas) {
+    console.warn('sendData ===> ', formDatas);
+    // sandbox 구현하기
+    this.vixxForm.reset();
+  }
+
+  // 공통 service 가능한지 추후 확인하기
+  public switchLanguage(lang: string): void {
+    this.translate.use(lang)
+    localStorage.setItem('selectedLanguage', lang)
+  }
 
   ngOnInit() {
   }
