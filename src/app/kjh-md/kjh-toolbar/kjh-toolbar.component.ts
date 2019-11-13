01242b64da1f5feacf29cd50cb74ab49b4a36842
jonkjh <creative_kjh@ibizsoftware.net>
2019-11-13 22:02:57 +0900

    firebase kjh

--- src/app/kjh-md/kjh-toolbar/kjh-toolbar.component.ts
+++ src/app/kjh-md/kjh-toolbar/kjh-toolbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { KjhMdSandbox } from '../kjh-md.sandbox';
+import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from '@angular/fire/firestore';
+import { AngularFireStorage} from '@angular/fire/storage';
+import { BehaviorSubject } from 'rxjs';
  
 @Component({
   selector: 'app-kjh-toolbar',
@@ -8,9 +11,26 @@ import { KjhMdSandbox } from '../kjh-md.sandbox';
 })
 export class KjhToolbarComponent implements OnInit {
 
+
+  //데이터 베이스 관련 객체
+  private DataBase : AngularFirestore;
+  private itemsCollection: AngularFirestoreCollection<any>;
+  private items : BehaviorSubject<any[]> = new BehaviorSubject([]);
+  //나중에 쓸 저장소 관련 객체
+  private storage : AngularFireStorage;
+
+
   constructor(
-    public kjhMdSandbox : KjhMdSandbox
-  ) { }
+    public kjhMdSandbox : KjhMdSandbox,
+    private afs: AngularFirestore,
+    private strg : AngularFireStorage
+  ) {
+    this.DataBase = afs;
+    this.storage = strg;
+    this.getItem('idol').subscribe((res)=>{  //idol 컬렉션에 대해서 구독행위 시작
+      console.log(res);
+    });
+   }
 
   ngOnInit() {
   }
@@ -21,5 +41,13 @@ export class KjhToolbarComponent implements OnInit {
     this.kjhMdSandbox.getAnUserDetailkjh(userId);
 
   }
+  getItem(db_name : string){
+    this.itemsCollection = this.DataBase.collection<any>(db_name, (ref) =>ref);  //컬렉션에 접속
+    this.itemsCollection.valueChanges().forEach((val)=>{  //데이터를 가져오기
+      this.items.next(val);  // 구독중인 대상에 데이터를 밀어주기
+    });    
+    return this.items;  //리턴
+  }
+
 
 }
